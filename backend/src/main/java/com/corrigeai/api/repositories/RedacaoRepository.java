476e622b8e152fdef3e6a987415023659c164e9a
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-11-17 19:03:19 -0300

    interface RedacaoRepository-comunica com o MongoDB

---
+++ backend/src/main/java/com/corrigeai/api/repositories/RedacaoRepository.java
@@ -0,0 +1,4 @@
+package com.corrigeai.api.repositories;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RedacaoRepository extends MongoRepository<Redacao, String> {}
