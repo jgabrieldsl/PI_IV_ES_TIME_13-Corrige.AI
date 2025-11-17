cf4a4af7babdd1e0f912caa1f8127585f1cba7e9
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-11-17 20:27:23 -0300

    arquivos: RedaçãoService e RedaçãoController

---
+++ backend/src/main/java/com/corrigeai/api/services/RedacaoService.java
@@ -0,0 +1,6 @@
+package com.corrigeai.api.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedacaoService {}
