1f033eb536b5b0c8de1384ef6ffc86af9ecd0993
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-11-17 20:27:23 -0300

    arquivos: RedaçãoService e RedaçãoController

---
+++ backend/src/main/java/com/corrigeai/api/controllers/RedacaoController.java
@@ -0,0 +1,8 @@
+package com.corrigeai.api.controllers;
+
+
+@RestController // retornar somente dados
+@RequestMapping("/api/redacoes") //define o prefixo de URL para todas as rotas desse controller
+public class RedacaoController{
+
+}
