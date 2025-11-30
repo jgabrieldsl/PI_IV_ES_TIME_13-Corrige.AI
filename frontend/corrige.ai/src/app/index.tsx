7eae97efc459cfb1a72b2691ea7381e6387bac61
jgabrieldsl <jgabriel.leite@outlook.com>
2025-11-30 17:45:05 -0300

    chore(app): add app entry and global styles

--- frontend/corrige.ai/src/app/index.tsx
+++ frontend/corrige.ai/src/app/index.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './auth/contexts/auth-context'
 import { Router } from './router'
+import { ThemeProvider } from '@/shared/components/theme-provider'
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark" storageKey="vite-ui-theme">
+      <AuthProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
